8ff9e22582c09350eb267ef8d511f4f461b1ac3d
Cielio Queiroz <cielioqueiroz@hotmail.com>
2026-06-05 09:09:13 -0300

    feat: secao Sobre com valores e contadores

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Hero from "@/components/sections/Hero";
+import Sobre from "@/components/sections/Sobre";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <Sobre />
       <Footer />
     </main>
   );
